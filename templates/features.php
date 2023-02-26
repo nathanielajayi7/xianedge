dd72e3ce5ec0683132e59cd48088cf90343621e1
nathanielajayi7 <nathanielajayi646@gmail.com>
2023-02-26 20:39:47 +0100

    Initial

---
+++ templates/features.php
@@ -0,0 +1,34 @@
+<section>
+<div class="md:flex md:-mx-4 mt-12 md:pt-4">
+      <div class="px-4 md:w-1/3 mt-6 md:mt-0">
+        <div class="feature-box text-center p-4 md:p-6 max-w-sm mx-auto border-2 border-solid border-gray-300 rounded md:h-full">
+          <div class="text-xl p-4 w-16 h-16 mx-auto">
+            <i class="fas fa-bolt text-indigo-600"></i>
+          </div>
+          <h5 class="text-xl font-medium mb-4">Security Training</h5>
+          <p class="text-gray-600 mb-3">To ensure that your team is ready to handle all types of security threats using our equipment.</p>
+        </div>
+      </div>
+      <div class="px-4 md:w-1/3 mt-6 md:mt-0">
+        <div class="feature-box text-center p-4 md:p-6 max-w-sm mx-auto border-2 border-solid border-gray-300 rounded md:h-full">
+          <div class="text-xl p-4 w-16 h-16 mx-auto">
+            <i class="fas fa-code text-indigo-600"></i>
+          </div>
+          <h5 class="text-xl font-medium mb-4">Maintenance & Support</h5>
+          <p class="text-gray-600 mb-3">For your convenience, we offer monthly service solutions on our security equipment.</p>
+        </div>
+      </div>
+      <div class="px-4 md:w-1/3 mt-6 md:mt-0">
+        <div class="feature-box text-center p-4 md:p-6 max-w-sm mx-auto border-2 border-solid border-gray-300 rounded md:h-full">
+          <div class="text-xl p-4 w-16 h-16 mx-auto">
+            <i class="fas fa-wrench text-indigo-600"></i>
+          </div>
+          <h5 class="text-xl font-medium mb-4">Security Equipment Rental</h5>
+          <p class="text-gray-600 mb-3">New and demo models are available on a rental agreement, to reduce your capital layout.</p>
+        </div>
+      </div>
+    </div>
+</div>
+
+<div class="mb-10">
+</section>
---
+++ templates/section_1.php
@@ -0,0 +1,100 @@
+<?
+
+$solutions = [
+    array(
+        "title" => "Baggage & Parcel Inspection",
+        "description" => "Light and compact security screening system, designed for smaller installations , making it
+        an excellent system for cruise lines, schools, prisons, and event-based security
+        environments.",
+        "image" => "images/baggage_inspector.jpeg"
+    ),
+
+    array(
+        "title" => "Baggage & Parcel Inspection",
+        "description" => "Light and compact security screening system, designed for smaller installations , making it
+        an excellent system for cruise lines, schools, prisons, and event-based security
+        environments.",
+        "image" => "images/hold_baggage_inspector.jpeg"
+    ),
+
+    array(
+        "title" => "Baggage & Parcel Inspection",
+        "description" => "Light and compact security screening system, designed for smaller installations , making it
+        an excellent system for cruise lines, schools, prisons, and event-based security
+        environments.",
+        "image" => "images/baggage_inspector.jpeg"
+    ),
+
+    array(
+        "title" => "Baggage & Parcel Inspection",
+        "description" => "Light and compact security screening system, designed for smaller installations , making it
+        an excellent system for cruise lines, schools, prisons, and event-based security
+        environments.",
+        "image" => "images/hold_baggage_inspector.jpeg"
+    ),
+
+];
+
+
+?>
+<section>
+    <div class="grid mx-auto justify-center items-center align-center py-8 px-8">
+
+        <p class="text-black align-center justify-center text-3xl font-extrabold dark:text-gray-400">We specialize in:
+        </p>
+        <hr class="h-px my-8 bg-gray-200 align-center items-center justify-center border-0 dark:bg-gray-700">
+        <p class="text-gray-500 text-xl dark:text-gray-400">
+            <span class="text-black">Rapiscan Systems' Security Screening Equipment
+                Fire Fighting & Security Screening Vehicles</span><br><br>
+            HAB International, based in Johannesburg, is the exclusive distributor of the industry leading Rapiscan
+            Security Screening Systems within Southern Africa.
+
+            We specialise in installing and supporting the full range of airport security, customs & border security,
+            and people screening equipment.
+        </p>
+    </div>
+
+    <div class="grid justify-center py-8 px-8 w-full">
+        <p class="text-black text-3xl font-extrabold w-full dark:text-gray-400">Our security solutions:
+        </p>
+        <hr class="h-px my-8 w-full bg-gray-200 border-0 dark:bg-gray-700">
+        <!-- <p class="text-gray-500 text-xl dark:text-gray-400">
+            <span class="text-black">Rapiscan Systems' Security Screening Equipment
+                Fire Fighting & Security Screening Vehicles</span><br><br>
+            HAB International, based in Johannesburg, is the exclusive distributor of the industry leading Rapiscan
+            Security Screening Systems within Southern Africa.
+
+            We specialise in installing and supporting the full range of airport security, customs & border security,
+            and people screening equipment.
+        </p> -->
+        <div>
+</section>
+<section class="mx-auto w-screen lg:p-8 sm:p-2">
+    <!-- Card -->
+    <div class="grid lg:grid-cols-3 lg:gap-6">
+
+        <?php foreach ($solutions as $solution) {
+
+            echo '<div class="max-w-md h-fit group">
+            <div class="relative overflow-hidden">
+                <img class="h-96 w-full object-scale-down"
+                    src="' . ($solution['image']) . '"
+                    alt="">
+                <div
+                    class="absolute grid h-full w-full bg-black/20 -bottom-10 pt-4 backdrop-blur-md bg-white/60 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300">
+                    <p class="font-semibold text-md text-black mx-2 py-2 px-5"><a href="#">' . ($solution['title']) . '</a><br><span class="text-md font-normal">
+                            <!-- <hr> -->
+                            ' . ($solution['description']) . '</span></p>
+                </div>
+                </div>
+            </div>';
+
+
+        }
+        ?>
+        <!-- <h2 class="mt-3 text-xl capitalize">Straw Hat</h2>
+    <del class="text-red-700 text-lg">$49</del>
+    <p class="text-xl mt-2 ml-1 inline-block">$35</p> -->
+    </div>
+    </div>
+</section>
