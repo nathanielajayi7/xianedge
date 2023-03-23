0fc9809b5ae1a3c48a003d98807d386fd0a431c2
nathanielajayi7 <nathanielajayi646@gmail.com>
2023-03-23 18:27:51 +0100

    host

--- config.php
+++ config.php
@@ -12,7 +12,7 @@ $db = initDb();
  */
 function isDebug(): bool
 {
-    return ($_SERVER['SERVER_NAME'] != 'products.xianedgeltd.com');
+    return str_contains(strtolower($_SERVER['SERVER_NAME']), 'xianedgeltd');
 }
 function initDb(): MysqliDb
 {
