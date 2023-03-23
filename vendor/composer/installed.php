c3832ea02a5686cdcd6d3d0be8900d0465c7e2b5
nathanielajayi7 <nathanielajayi646@gmail.com>
2023-03-23 18:13:24 +0100

    vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nath/xianedge',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'dd72e3ce5ec0683132e59cd48088cf90343621e1',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nath/xianedge' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'dd72e3ce5ec0683132e59cd48088cf90343621e1',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'thingengineer/mysqli-database-class' => array(
+            'pretty_version' => 'v2.9.3',
+            'version' => '2.9.3.0',
+            'reference' => '03dbce1c7f1c9def315ad8ef52e1f5c0a806d6ed',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../thingengineer/mysqli-database-class',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
