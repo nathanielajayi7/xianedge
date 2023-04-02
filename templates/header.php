6de349bc4eab3045097ecf6542fb239b25626f14
nathanielajayi7 <nathanielajayi646@gmail.com>
2023-04-02 11:00:36 +0100

    rel

--- templates/header.php
+++ templates/header.php
@@ -4,23 +4,20 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="
-<?php
+  <script src="<?php
 echo __DIR__;
 ?>/scripts/tailwinds.js"></script>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
     rel="stylesheet">
-  <link rel="stylesheet" href="
-<?php
+  <link rel="stylesheet" href="<?php
 echo __DIR__;
 ?>/css/custom.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
     integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <script src="
-<?php
+  <script src="<?php
 echo __DIR__;
 ?>/scripts/config.tailwinds.js"></script>
 
